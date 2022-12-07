5e32e4523b40ca9fa01921f35946ee6062c12fe1
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:34:23 +0900

    이름추가

--- group4getLotto/src/group4getLotto/Lotto.java
+++ group4getLotto/src/group4getLotto/Lotto.java
@@ -4,11 +4,11 @@ public class Lotto {
 
 	public static void main(String[] args) {
 		System.out.println("김주용");
+		System.out.println("김경연");
 		// lotto
 		// 영자면옥 맛있다.
 		// 오~ 영자면옥~
 		// 오~ 먹어봄 
-		// 김경연
 		
 	}
 
