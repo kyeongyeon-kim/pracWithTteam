c0b0398421200a035743559346aaa4a84852d52c
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:40:24 +0900

    ㅊ

--- group4getLotto/src/group4getLotto/Lotto.java
+++ group4getLotto/src/group4getLotto/Lotto.java
@@ -4,16 +4,13 @@ public class Lotto {
 
 	public static void main(String[] args) {
 		System.out.println("김주용");
-<<<<<<< HEAD
 		System.out.println("김주용");
-=======
 		System.out.println("김경연");
->>>>>>> branch 'master' of https://github.com/yeon966/pracWithTteam.git
+		System.out.println("김경연");
 		// lotto
 		// 영자면옥 맛있다.
 		// 오~ 영자면옥~
 		// 오~ 먹어봄 
-		
 	}
 
 }
