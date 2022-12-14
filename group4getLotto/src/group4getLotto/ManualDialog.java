2d14ed4df087899a29826f19a14a6607321cbafe
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-14 14:07:56 +0900

    이미지 정리 및 경로 재설정 작업

---
+++ group4getLotto/src/group4getLotto/ManualDialog.java
@@ -0,0 +1,38 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+class ImagePanel extends JPanel {
+	private Image image;
+
+	public ImagePanel(Image image) {
+		this.image = image;
+		setSize(new Dimension(image.getWidth(null), image.getHeight(null)));
+		setLayout(null);
+
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(image, 0, 0, null);
+	}
+
+}
+
+public class ManualDialog extends JDialog {
+
+	public ManualDialog() {
+		ImagePanel panel = new ImagePanel(new ImageIcon("/inputColor/manual.jpg").getImage());
+		add(panel);
+		pack();
+		
+		setSize(450, 550);
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+	}
+
+}
